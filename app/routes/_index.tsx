419f7e87348fd85614572360fd0db58f2366a8e4
Paqui <paqui.calabria@adevinta.com>
2024-06-06 21:32:53 +0200

    Max width

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -23,31 +23,37 @@ export default function Index() {
   };
 
   return (
-    <div className='min-h-screen bg-gradient-to-b from-sky-100 via-green-50 to-yellow-50 py-4 px-3'>
-      <h1 className='text-3xl font-bold text-slate-800 pb-4'>Hike fuel</h1>
-      <NutritionalInfo
-        onNutritionChange={(nutritionInfo) => {
-          const safeGet = safeGetter(nutritionInfo);
-          const calculatedCals =
-            safeGet('fats') * 9 + safeGet('carbs') * 4 + safeGet('protein') * 4;
-          setDensity(safeGet('energy') / safeGet('quantity'));
-          setCarbProt(safeGet('carbs') / safeGet('protein'));
-          setCalFat((safeGet('fats') * 9 * 100) / calculatedCals);
-          setCalSugar((safeGet('sugar') * 4 * 100) / calculatedCals);
-          setSalt((safeGet('salt') * 1000) / safeGet('quantity'));
-        }}
-        onTitleChange={(newTitle) => {
-          setFoods([{ ...foods[0], title: newTitle }]);
-        }}
-      />
-      <h2 className='text-xl font-bold text-slate-800 pt-4 pb-2'>Evaluación</h2>
-      <NutritionalResults
-        density={density}
-        carbProt={carbProt}
-        calFat={calFat}
-        calSugar={calSugar}
-        salt={salt}
-      />
+    <div className='min-h-screen bg-gradient-to-b from-sky-100 via-green-50 to-yellow-50  py-4 px-3'>
+      <div className='max-w-2xl mx-auto'>
+        <h1 className='text-3xl font-bold text-slate-800 pb-4'>Hike fuel</h1>
+        <NutritionalInfo
+          onNutritionChange={(nutritionInfo) => {
+            const safeGet = safeGetter(nutritionInfo);
+            const calculatedCals =
+              safeGet('fats') * 9 +
+              safeGet('carbs') * 4 +
+              safeGet('protein') * 4;
+            setDensity(safeGet('energy') / safeGet('quantity'));
+            setCarbProt(safeGet('carbs') / safeGet('protein'));
+            setCalFat((safeGet('fats') * 9 * 100) / calculatedCals);
+            setCalSugar((safeGet('sugar') * 4 * 100) / calculatedCals);
+            setSalt((safeGet('salt') * 1000) / safeGet('quantity'));
+          }}
+          onTitleChange={(newTitle) => {
+            setFoods([{ ...foods[0], title: newTitle }]);
+          }}
+        />
+        <h2 className='text-xl font-bold text-slate-800 pt-4 pb-2'>
+          Evaluación
+        </h2>
+        <NutritionalResults
+          density={density}
+          carbProt={carbProt}
+          calFat={calFat}
+          calSugar={calSugar}
+          salt={salt}
+        />
+      </div>
     </div>
   );
 }
